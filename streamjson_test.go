d44a480485ffe1ea8b37fa157e6c8dd7656c41a9
button-chen <727690080@qq.com>
2024-05-24 06:36:05 +0800

    Update streamjson_test.go

--- streamjson_test.go
+++ streamjson_test.go
@@ -15,30 +15,33 @@ func TestStreamJson(t *testing.T) {
 	js := `{"aa":{"a":1,"b":{"c":2,"d":5}},"bb":[{"a":123},{"a":456}],"cc":[["e","f"],["g","h"]], "ee":999}`
 
 	go func() {
-		// 模拟流式发送数据
+		// 模拟流式发送json数据
+		now := time.Now()
 		for i := 0; i < len(js); i++ {
 			cw.Write(append([]byte(nil), js[i]))
 			time.Sleep(time.Millisecond * 10)
 		}
+		fmt.Printf("send end: %vms\n", time.Since(now).Milliseconds())
 		cw.Close()
 	}()
 
 	sj := NewStreamJson()
 
+	start := time.Now()
 	sj.AddMonitor("name", func(a any, err error) {
-		fmt.Printf("timestamp: %v name: %v  error: %v\n", time.Now().UnixMilli(), a, err)
+		fmt.Printf("timestamp: %v name: %v  error: %v\n", time.Since(start).Milliseconds(), a, err)
 	})
 	sj.AddMonitor("ee", func(a any, err error) {
-		fmt.Printf("timestamp: %v ee: %v  error: %v\n", time.Now().UnixMilli(), a, err)
+		fmt.Printf("timestamp: %v ee: %v  error: %v\n", time.Since(start).Milliseconds(), a, err)
 	})
 	sj.AddMonitor("aa.b.c", func(a any, err error) {
-		fmt.Printf("timestamp: %v aa.b.c: %v  error: %v\n", time.Now().UnixMilli(), a, err)
+		fmt.Printf("timestamp: %v aa.b.c: %v  error: %v\n", time.Since(start).Milliseconds(), a, err)
 	})
 	sj.AddMonitor("cc.*.*", func(a any, err error) {
-		fmt.Printf("timestamp: %v cc.*.*: %v  error: %v\n", time.Now().UnixMilli(), a, err)
+		fmt.Printf("timestamp: %v cc.*.*: %v  error: %v\n", time.Since(start).Milliseconds(), a, err)
 	})
 	sj.AddMonitor("bb.*.a", func(a any, err error) {
-		fmt.Printf("timestamp: %v bb.*.a: %v  error: %v\n", time.Now().UnixMilli(), a, err)
+		fmt.Printf("timestamp: %v bb.*.a: %v  error: %v\n", time.Since(start).Milliseconds(), a, err)
 	})
 
 	err := sj.ProcessStream(cr)
